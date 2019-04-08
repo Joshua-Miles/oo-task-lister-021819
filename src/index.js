916419766f28165741276dcbb9175e9a2d2e4503
joshua-miles <joshua@christetulit.org>
2019-04-08 17:46:27 -0500

    something

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
+  const form = document.querySelector('#create-task-form')
+  const taskDescriptionInput = document.querySelector('#new-task-description')
+  const listContainer = document.querySelector("#list");
+
   const taskList = new TaskList();
-
-  const listContainer = document.getElementById("list");
-  const renderApp = () => (listContainer.innerHTML = taskList.render());
-
+  listContainer.append(taskList.element)
+  
   // Add Form Behavior Here!
+  form.addEventListener('submit', function(e){
+    e.preventDefault()
+    const description = taskDescriptionInput.value
+    taskList.createItem(description)
+    taskDescriptionInput.value = ''
+  })
 
-  renderApp()
 });
--- src/taskList.js
+++ src/taskList.js
@@ -1,3 +1,13 @@
+// render a ul
 class TaskList {
-  
+    
+    constructor(){
+        this.element = document.createElement('ul')
+    }
+
+    createItem(description){
+        let item = new TaskListItem(description)
+        this.element.append(item.element)
+    }
+
 }
